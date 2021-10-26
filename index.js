8688864bf7951e8060b7304c2cac1a902a84c937
sagorhasanmehedi <mehedihasansagor1995@gmail.com>
2021-10-27 02:37:16 +0600

    added porpus

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("server is running");
+  res.send("this is chaking purpose for heroku");
 });
 
 app.listen(port, () => {
